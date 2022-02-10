d5f981bef449b9c851084d9b8314c15684d9e1d2
andrew-peterson0429 <andrew.peterson1021@gmail.com>
2022-02-10 14:11:31 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+ <div id="app">
+
+ </div>
+</template>
+./components/ExerciseApp.vue
+<script>
+import ExerciseApp from './components/ExerciseApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ExerciseApp
+  }
+}
+</script>
+
+<style>
+
+</style>
