7945275da0928597bbc73079bb287123f1358a6b
andrew-peterson0429 <andrew.peterson1021@gmail.com>
2022-02-11 15:44:57 -0700

    added inputs and methods to combine totals

--- README.md
+++ README.md
@@ -22,3 +22,38 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+## Exercise Tracking App Assignment -  Requirements
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+* Add Exercises to Track
+  * Example exercises: Running (time), bench press (sets, reps, and weight)
+* Track a user-entered exercise by time and distance or sets, reps, and weight amount
+* See a history of past exercises and workouts with tracked stats
+* Be able to set a date when the exercise was performed
+* Must be hosted at a live URL
+### Compiles and minifies for production
+```
+npm run build
+```
+
+When complete, turn in a link to your Github classroom repo and the live-hosted URL The live site should be in the URL submission.
+### Lints and fixes files
+```
+npm run lint
+```
+
+## Tips
+* Work through the tutorial on the VueJS.org website.
+* Note, the tutorial is good at examples and concepts, but not so great about how it all fits together.
+* Use The Vue CLI to bootstrap your project
+* Remember Vue is a framework, it is very opinionated about how you structure your app and implement your features.
+* A database is not required for this assignment. You can just store the data in memory and lose the data on page refresh.
+* I would recommend starting with a basic data model that is always there on page load.
+  * If you do want to implement a sudo database, there are JSON APIs that could work very well such as: https://mockend.com (Links to an external site.) or https://fakejson.com (Links to an external site.) 
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/ExerciseApp.vue
+++ src/components/ExerciseApp.vue
@@ -1,78 +1,125 @@
 <template>
   <div class="container">
-    <h2 class="text-center mt-5">Vue Exercise App</h2>
+    <h2 class="text-center mt-5">Vue Exercise Tracking App</h2>
     
    <!--  Input -->
-   <div class="d-flex ">
-     <input v-model="task" type="text" placeholder="Enter Text" class="form-control">
-
-     <button @click="submitTask()" class="btn btn-warning rounded-0">Submit</button>
+   <div class="d-flex mt-5">
+      <input v-model="task" type="text" placeholder="Enter Exercise" class="form-control">
+      <input v-model="rep" type="text" placeholder="Enter Reps" class="form-control">
+      <input v-model="distance" type="text" placeholder="Enter Distance" class="form-control">
+      <input v-model="time" type="text" placeholder="Enter Time" class="form-control">
+      <input type="date" value="2017-06-01">
+      <button @click="submitTask()" class="btn btn-warning rounded-0">Submit</button>
    </div>
 
-  <!-- Table -->
-     <table class="table table-bordered mt-5">
+      <!-- Table -->
+        <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Workout</th>
+          <th scope="col">Reps</th>
+          <th scope="col">Distance</th>
+          <th scope="col">Time</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center"></th>
+          <th scope="col" class="text-center"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <th>
+            <span :class="{'finished': task.status === 'Finished'}">{{task.name}}</span>
+            </th>
+            <td>
+              {{task.rep}}
+            </td>
+            <td>
+              {{task.distance}}
+            </td>
+            <td>
+              {{task.time}}
+            </td>
+          <td style="width: 120px">
+            <span @click="changeStatus(index)" class="pointer" 
+            :class="{'text-danger': task.status === 'To-do',
+            'text-warning': task.status === 'In-progress',
+            'text-success': task.status === 'Finished'}">
+            {{task.status}}
+            </span>
+            </td>
+          <td>
+            <div class="text-center pointer" @click="editTask(index)">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div class="text-center pointer" @click="deleteTask(index)">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+   
+    <table class="table mt-5">
   <thead>
     <tr>
-      <th scope="col">Task</th>
-      <th scope="col">Status</th>
-      <th scope="col" class="text-center">#</th>
-      <th scope="col" class="text-center">#</th>
+      <th scope="col">Total Workouts</th>
+      <th scope="col">Total Reps</th>
+      <th scope="col">Total Distance</th>
+      <th scope="col">Total Time</th>
     </tr>
   </thead>
   <tbody>
-    <tr v-for="(task, index) in tasks" :key="index">
-      <th>
-        <span :class="{'finished': task.status === 'Finished'}">{{task.name}}</span>
-        </th>
-      <td style="width: 120px">
-        <span @click="changeStatus(index)" class="pointer" 
-        :class="{'text-danger': task.status === 'To-do',
-        'text-warning': task.status === 'In-progress',
-        'text-success': task.status === 'Finished'}">
-        {{task.status}}
-        </span>
-        </td>
-      <td>
-        <div class="text-center " @click="editTask(index)">
-          <span class="fa fa-pen"></span>
-        </div>
-      </td>
-      <td>
-        <div class="text-center" @click="deleteTask(index)">
-          <span class="fa fa-trash"></span>
-        </div>
-      </td>
+    <tr>
+      <th scope="row">{{totalWorkouts}}</th>
+      <td>{{totalReps}}</td>
+      <td>{{totalDistance}}</td>
+      <td>{{totalTime}}</td>
     </tr>
+    
   </tbody>
 </table>
-   
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-
   data() {
     return {
       task: '',
       editedTask: null,
+      rep: '',
+      editedRep: null,
+      distance: '',
+      editedDistance: null,
+      time: '',
+      editedTime: null,
       availableStatuses: ['To-do', 'In-progress', 'Finished'],
 
       tasks: [
-        {
-          name: 'steal banana',
-          status: 'To-do'
+        /* {
+          name: 'Push ups',
+          status: 'To-do',
+          rep: 1,
+          distance: 2,
+          time: 3
         },
         {
-          name: 'eat 1kg of chocolate',
-          status: 'In-progress'
-        },
+          name: 'Jog',
+          status: 'In-progress',
+          rep: 4,
+          distance: 5,
+          time: 6
+        }, */
         
-      ]
+      ],
+
+      totalWorkouts: 0,
+      totalReps: 0,
+      totalDistance: 0,
+      totalTime: 0,
     }
   },
 
@@ -83,16 +130,24 @@ export default {
       if(this.editedTask === null) {
           this.tasks.push({
           name: this.task,
+          rep: this.rep,
+          time: this.time,
+          distance: this.distance,
           status: 'To-do'
         });
       } else {
         this.tasks[this.editedTask].name = this.task;
+        this.tasks[this.editedTask].rep = this.rep;
+        this.tasks[this.editedTask].distance = this.distance;
+        this.tasks[this.editedTask].time = this.time;
         this.editedTask = null;
       }
 
-      
-
       this.task = '';
+      this.rep = '';
+      this.distance = '';
+      this.time = '';
+      this.totals();
     },
 
     deleteTask(index) {
@@ -102,15 +157,38 @@ export default {
      editTask(index) {
       this.task = this.tasks[index].name;
       this.editedTask = index;
+      this.rep = this.tasks[index].rep;
+      this.distance = this.tasks[index].distance;
+      this.time = this.tasks[index].time;
+      
+      
      },
 
      changeStatus(index) {
        let newIndex = this.availableStatuses.indexOf(this.tasks[index].status);
        if (++ newIndex > 2) newIndex = 0;
        this.tasks[index].status = this.availableStatuses[newIndex];
-     }
+     },
     
-  }
+    totals() {
+      this.totalWorkouts = 0;
+      this.totalReps = 0;
+      this.totalDistance = 0;
+      this.totalTime = 0;
+    
+      for (let i = 0; i < this.tasks.length; i++) {
+        this.totalWorkouts += 1;
+        this.totalReps += parseInt(this.tasks[i].rep);
+        this.totalDistance += parseInt(this.tasks[i].distance);
+        this.totalTime += parseInt(this.tasks[i].time);
+        console.log(this.tasks);
+      }
+      
+    }
+
+  },
+
+  
 
 };
 
